09f1128381014c8427e6f114848afbc651176c1a
yafred <fred599@laposte.net>
2016-11-13 20:31:25 +0100

    refactor post_id to postId

--- map/explore.js
+++ map/explore.js
@@ -112,7 +112,7 @@
 			if(e.target.postId != stateObj.selectedPostId) {
 				markers[e.target.postId].setIcon(markerHoverIcon);
 				markers[e.target.postId]._bringToFront();
-				$("div.postContent[data-post_id=" + e.target.postId + "]").addClass('hover');
+				$("div.postContent[data-postId=" + e.target.postId + "]").addClass('hover');
 			}
 
 		});
@@ -121,7 +121,7 @@
 			map.closePopup(tooltipPopup);
 
 			// Style marker and post in postlist
-			$("div.postContent[data-post_id=" + e.target.postId + "]").removeClass('hover');
+			$("div.postContent[data-postId=" + e.target.postId + "]").removeClass('hover');
 			if(e.target.postId != stateObj.selectedPostId) {
 				markers[e.target.postId]._resetZIndex();
 				markers[e.target.postId].setIcon(markerIcon);
@@ -148,7 +148,7 @@
 	function popupClosed(e) {
 		if(e.popup === stickyPopup) {
 			if (stateObj.selectedPostId != -1) {
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").removeClass("selected");
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").removeClass("selected");
 				markers[stateObj.selectedPostId]._resetZIndex();
 				markers[stateObj.selectedPostId].setIcon(markerIcon);
 				stateObj.selectedPostId = -1;
@@ -163,26 +163,26 @@
 		var doShowTooltip = false;
 		if (stateObj.selectedPostId == -1) {
 			stateObj.selectedPostId = e.target.postId;
-			$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").removeClass("hover");
-			$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").addClass("selected");
+			$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").removeClass("hover");
+			$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").addClass("selected");
 			markers[stateObj.selectedPostId].setIcon(markerSelectedIcon);
 			markers[stateObj.selectedPostId]._bringToFront();
 		}
 		else {
 			if(stateObj.selectedPostId == e.target.postId) {
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").removeClass("selected");
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").addClass("hover");
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").removeClass("selected");
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").addClass("hover");
 				markers[stateObj.selectedPostId].setIcon(markerHoverIcon);
 				markers[stateObj.selectedPostId]._bringToFront();
 				stateObj.selectedPostId = -1;
 				doShowTooltip = true;
 			}
 			else {
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").removeClass("selected");
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").removeClass("selected");
 				markers[stateObj.selectedPostId]._resetZIndex();
 				markers[stateObj.selectedPostId].setIcon(markerIcon);
 				stateObj.selectedPostId = e.target.postId;
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").addClass("selected");
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").addClass("selected");
 				markers[stateObj.selectedPostId].setIcon(markerSelectedIcon);
 				markers[stateObj.selectedPostId]._bringToFront();
 			}
@@ -199,24 +199,24 @@
 	}
 	
 	// Center map on post
-	function centerMapOnPost(post_id) {
-		map.setView(markers[post_id].getLatLng(), map.getZoom());
+	function centerMapOnPost(postId) {
+		map.setView(markers[postId].getLatLng(), map.getZoom());
 
 		if (stateObj.selectedPostId == -1) {
-			stateObj.selectedPostId = post_id;
-			$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").removeClass("hover");
-			$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").addClass("selected");
+			stateObj.selectedPostId = postId;
+			$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").removeClass("hover");
+			$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").addClass("selected");
 			markers[stateObj.selectedPostId].setIcon(markerSelectedIcon);
 			markers[stateObj.selectedPostId]._bringToFront();
 		}
 		else {
-			if(stateObj.selectedPostId != post_id) {
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").removeClass("selected");
+			if(stateObj.selectedPostId != postId) {
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").removeClass("selected");
 				markers[stateObj.selectedPostId]._resetZIndex();
 				markers[stateObj.selectedPostId].setIcon(markerIcon);
-				stateObj.selectedPostId = post_id;
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").removeClass("hover");
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").addClass("selected");
+				stateObj.selectedPostId = postId;
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").removeClass("hover");
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").addClass("selected");
 				markers[stateObj.selectedPostId].setIcon(markerSelectedIcon);
 				markers[stateObj.selectedPostId]._bringToFront();
 			}
@@ -242,7 +242,7 @@
 					
 			// initial view
 			if(stateObj.selectedPostId != -1 && markers[stateObj.selectedPostId]) {
-				$("div.postContent[data-post_id=" + stateObj.selectedPostId + "]").addClass("selected");
+				$("div.postContent[data-postId=" + stateObj.selectedPostId + "]").addClass("selected");
 				markers[stateObj.selectedPostId].setIcon(markerSelectedIcon);
 				markers[stateObj.selectedPostId]._bringToFront();
 				updateStickyPopup();						
@@ -266,30 +266,30 @@
 		$("div.postContent").on("click", postClicked);
 		
 		$("div.postContent").on("mouseenter", function(e) {
-			var post_id = $(this).attr("data-post_id");
-			if(post_id != stateObj.selectedPostId) {
+			var postId = $(this).attr("data-postId");
+			if(postId != stateObj.selectedPostId) {
 				tooltipPopup = new L.Rrose({ offset: new L.Point(0,-10), closeButton: false, autoPan: false });	
-				var title = postlistByGlobalId[post_id].title;
+				var title = postlistByGlobalId[postId].title;
 				tooltipPopup.setContent(title);
-				tooltipPopup.setLatLng(markers[post_id].getLatLng());
+				tooltipPopup.setLatLng(markers[postId].getLatLng());
 				tooltipPopup.openOn(map);
 
 				
-				markers[post_id].setIcon(markerHoverIcon);
-				markers[post_id]._bringToFront();
+				markers[postId].setIcon(markerHoverIcon);
+				markers[postId]._bringToFront();
 				$(this).addClass('hover');
 			}
 			$(this).find(".cmdContainer").show();
 		});
 					
 		$("div.postContent").on("mouseleave", function(e) {
-			var post_id = $(this).attr("data-post_id");
+			var postId = $(this).attr("data-postId");
 			$(this).removeClass('hover');
-			if(post_id != stateObj.selectedPostId) {
+			if(postId != stateObj.selectedPostId) {
 				map.closePopup(tooltipPopup);
 
-				markers[post_id]._resetZIndex();
-				markers[post_id].setIcon(markerIcon);
+				markers[postId]._resetZIndex();
+				markers[postId].setIcon(markerIcon);
 			}
 			$(this).find(".cmdContainer").hide();
 		});
@@ -299,8 +299,8 @@
 				event.stopPropagation();
 				event.preventDefault();				
 
-				var post_id = $(this).attr("data-post_id");
-				centerMapOnPost(post_id);
+				var postId = $(this).attr("data-postId");
+				centerMapOnPost(postId);
 			});
 		
 	}
@@ -308,10 +308,10 @@
 	
 	// Post div clicked
 	function postClicked(e) {
-		var post_id = $(this).attr("data-post_id");
+		var postId = $(this).attr("data-postId");
 
 		$(this).append("<div class='loading'>");
-		stateObj.selectedPostId = post_id;
+		stateObj.selectedPostId = postId;
 		updateHistory();
 
 		// track if possible
@@ -319,23 +319,23 @@
 			ga('send', 'event', {
 			    eventCategory: 'Outbound Link',
 			    eventAction: 'click',
-			    eventLabel: postlistByGlobalId[post_id].title,
+			    eventLabel: postlistByGlobalId[postId].title,
 			    hitCallback: function() {
-			      window.location = postlistByGlobalId[post_id].url;
+			      window.location = postlistByGlobalId[postId].url;
 			    }
 			  });
 		}
 		else {
-			window.location = postlistByGlobalId[post_id].url;
+			window.location = postlistByGlobalId[postId].url;
 		}
 	}
 	
 
 	// Show tooltip of postId
-	function showTooltip(post_id) {
+	function showTooltip(postId) {
 		tooltipPopup = new L.Rrose({ offset: new L.Point(0,-10), closeButton: false, autoPan: false });		
-		tooltipPopup.setContent(Mustache.render(tooltipTpl, postlistByGlobalId[post_id]) );
-		tooltipPopup.setLatLng(markers[post_id].getLatLng());
+		tooltipPopup.setContent(Mustache.render(tooltipTpl, postlistByGlobalId[postId]) );
+		tooltipPopup.setLatLng(markers[postId].getLatLng());
 		tooltipPopup.openOn(map);
 	}
 	
@@ -352,7 +352,7 @@
 			stickyPopup = new L.Rrose({ offset: new L.Point(0,-10), closeButton: false, autoPan: false, className: 'sticky' });	
 			stickyPopup.setContent(Mustache.render(stickyTooltipTpl, postlistByGlobalId[stateObj.selectedPostId]) );
 			stickyPopup.setLatLng(markers[stateObj.selectedPostId].getLatLng());
-			stickyPopup.post_id = stateObj.selectedPostId;
+			stickyPopup.postId = stateObj.selectedPostId;
 			map.addLayer(stickyPopup);
 		}
 	}
@@ -471,7 +471,7 @@
 		var padding = parseInt($("#page").css("padding-top")) + parseInt($(".postContent").css("margin-top"));
 
 		if (stateObj.selectedPostId != -1) {
-		    var scrollTo = $("div.postContent[data-post_id=" + stateObj.selectedPostId + "]");
+		    var scrollTo = $("div.postContent[data-postId=" + stateObj.selectedPostId + "]");
 			
 			if(scrollTo.offset()) {
 				container.animate({
