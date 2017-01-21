5988549cb79be40e53002d816298a2fa1a648bf3
yafred <fred599@laposte.net>
2017-01-21 17:23:33 +0100

    add videoturysta

--- map/data/channels.js
+++ map/data/channels.js
@@ -8,5 +8,6 @@ channels = [
 	{ 'id': 'des-racines-et-des-ailes', 'title': 'Des Racines Et Des Ailes', 'url': 'data/des-racines-et-des-ailes-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-GW91M4y1cyw/AAAAAAAAAAI/AAAAAAAAAAA/dNlORfKxUtw/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'philippe-croom', 'title': 'Philippe Croom', 'url': 'data/philippe-croom-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-Yk44fDchtjM/AAAAAAAAAAI/AAAAAAAAAAA/LlRIlTs0zU4/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'le-chemin-de-ronde', 'title': 'Le Chemin de Ronde', 'url': 'data/le-chemin-de-ronde-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-s2Jt5fVACJo/AAAAAAAAAAI/AAAAAAAAAAA/dP779_oxkd8/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
-	{ 'id': 'la-cle-de-voute', 'title': 'La Clef de Voute', 'url': 'data/la-cle-de-voute-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-s6ijuK5Ax84/AAAAAAAAAAI/AAAAAAAAAAA/kHU5p8va5x4/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' }	
+	{ 'id': 'la-cle-de-voute', 'title': 'La Clef de Voute', 'url': 'data/la-cle-de-voute-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-s6ijuK5Ax84/AAAAAAAAAAI/AAAAAAAAAAA/kHU5p8va5x4/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
+	{ 'id': 'video-turysta', 'title': 'VideoTurysta', 'url': 'data/video-turysta-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-ha8Iiz91Oog/AAAAAAAAAAI/AAAAAAAAAAA/dJTOHK_kAwM/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' }	
 ];
