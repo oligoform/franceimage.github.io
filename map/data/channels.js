fddbd78a35f2b1cb6a07c5a8bb0864da491e1b75
yafred <fred599@laposte.net>
2017-01-22 12:28:53 +0100

    merge cle-de-voute and chemin-de-ronde

--- map/data/channels.js
+++ map/data/channels.js
@@ -7,7 +7,6 @@ channels = [
 	{ 'id': 'midi-en-france-suivez-le-guide', 'title': 'Midi en France - Suivez le Guide', 'url': 'data/midi-en-france-suivez-le-guide-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-M1B6zdM6rWE/AAAAAAAAAAI/AAAAAAAAAAA/b8BK0_U1lCg/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'des-racines-et-des-ailes', 'title': 'Des Racines Et Des Ailes', 'url': 'data/des-racines-et-des-ailes-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-GW91M4y1cyw/AAAAAAAAAAI/AAAAAAAAAAA/dNlORfKxUtw/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'philippe-croom', 'title': 'Philippe Croom', 'url': 'data/philippe-croom-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-Yk44fDchtjM/AAAAAAAAAAI/AAAAAAAAAAA/LlRIlTs0zU4/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
-	{ 'id': 'le-chemin-de-ronde', 'title': 'Le Chemin de Ronde', 'url': 'data/le-chemin-de-ronde-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-s2Jt5fVACJo/AAAAAAAAAAI/AAAAAAAAAAA/dP779_oxkd8/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
-	{ 'id': 'la-cle-de-voute', 'title': 'La Clef de Voute', 'url': 'data/la-cle-de-voute-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-s6ijuK5Ax84/AAAAAAAAAAI/AAAAAAAAAAA/kHU5p8va5x4/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
+	{ 'id': 'cle-de-voute-chemin-de-ronde', 'title': 'La Clef de Voute - Le Chemin de Ronde', 'url': 'data/cle-de-voute-chemin-de-ronde-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-s6ijuK5Ax84/AAAAAAAAAAI/AAAAAAAAAAA/kHU5p8va5x4/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'video-turysta', 'title': 'VideoTurysta', 'url': 'data/video-turysta-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-ha8Iiz91Oog/AAAAAAAAAAI/AAAAAAAAAAA/dJTOHK_kAwM/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' }	
 ];
