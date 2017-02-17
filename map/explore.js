ea1c48474534d03dc2c74267460a45a4933cd5ad
yafred <fred599@laposte.net>
2017-02-17 13:38:51 +0100

    track switch channel

--- map/explore.js
+++ map/explore.js
@@ -210,17 +210,17 @@
 	
 	
 	function switchToChannel(channelId, triggeredFromChannelsView) {
-		// track if possible
-		if(triggeredFromChannelsView && typeof ga == 'function') { 
-			ga('send', 'event', {
-			    eventCategory: 'channel',
-			    eventAction: 'open',
-			    eventLabel: channelId
-			});
-		}
-
 		var triggeredFromChannelsView = (typeof triggeredFromChannelsView !== 'undefined') ?  triggeredFromChannelsView : true;
 		if(triggeredFromChannelsView) {
+			// track if possible
+			if(typeof ga == 'function') { 
+				ga('send', 'event', {
+				    eventCategory: 'channel',
+				    eventAction: 'open',
+				    eventLabel: channelId
+				});
+			}
+
 			stateObj.selectedPostId = -1;
 		}
 		
